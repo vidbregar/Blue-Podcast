db7ec57deda147b901a9649f41f0e3aee4341876
Vid Bregar <vid.bregar@gmail.com>
2018-07-25 17:26:00 +0200

    Add search network endpoint

--- BluePodcast/app/src/main/java/com/example/vidbregar/bluepodcast/model/network/PodcastService.java
+++ BluePodcast/app/src/main/java/com/example/vidbregar/bluepodcast/model/network/PodcastService.java
@@ -2,27 +2,30 @@ package com.example.vidbregar.bluepodcast.model.network;
 
 import com.example.vidbregar.bluepodcast.model.data.Podcast;
 import com.example.vidbregar.bluepodcast.model.data.PodcastGenre;
+import com.example.vidbregar.bluepodcast.model.data.Search;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
-import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface PodcastService {
 
-    @Headers("Accept: application/json")
     @GET("api/v1/best_podcasts")
     Call<PodcastGenre> getBestPodcasts(@Header("X-Mashape-Key") String xMashapeKey);
 
-    @Headers("Accept: application/json")
     @GET("api/v1/best_podcasts")
     Call<PodcastGenre> getGenrePodcasts(@Header("X-Mashape-Key") String xMashapeKey,
                                         @Query("genre_id") int genreId);
 
-    @Headers("Accept: application/json")
     @GET("api/v1/podcasts/{podcastId}")
     Call<Podcast> getPodcast(@Header("X-Mashape-Key") String xMashapeKey,
                              @Path("podcastId") String podcastId);
+
+
+    @GET("api/v1/search")
+    Call<Search> search(@Header("X-Mashape-Key") String xMashapeKey,
+                        @Query("q") String query);
+
 }
